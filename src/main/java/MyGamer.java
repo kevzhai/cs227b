c7db853d52c900d74a48a596c256b176ffab0d20
Kevin Zhai <kevzhai@users.noreply.github.com>
2017-04-16 13:31:11 -0700

    player connectining

---
+++ src/main/java/MyGamer.java
@@ -0,0 +1,76 @@
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class MyGamer extends StateMachineGamer {
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		// TODO Auto-generated method stub
+		return new ProverStateMachine();
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+		long start = System.currentTimeMillis();
+
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move selection = (moves.get(new Random().nextInt(moves.size())));
+
+		long stop = System.currentTimeMillis();
+
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Kilimanjaro";
+	}
+
+}
