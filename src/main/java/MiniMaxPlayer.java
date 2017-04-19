1fff67bcda13ede4d7ee006c47e5fb03ca8b2663
Kevin Zhai <kevzhai@users.noreply.github.com>
2017-04-18 17:32:22 -0700

    minimax working for one opponent

--- src/main/java/MiniMaxPlayer.java
+++ src/main/java/MiniMaxPlayer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ggp.base.apps.player.detail.DetailPanel;
@@ -6,12 +7,13 @@ import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
-
 public class MiniMaxPlayer extends StateMachineGamer {
 
 	@Override
@@ -34,12 +36,57 @@ public class MiniMaxPlayer extends StateMachineGamer {
 		long start = System.currentTimeMillis();
 
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-		Move selection = (moves.get(0));
+		Move bestMove = (moves.get(0));
+		int bestScore = 0;
+		Role role = getRole();
+		MachineState currentState = getCurrentState();
+		for (Move move: moves) {
+			int result = getMinScore(role, move, currentState);
+			if (result > bestScore) {
+				result = bestScore;
+				bestMove = move;
+			}
+		}
 
 		long stop = System.currentTimeMillis();
 
-		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
-		return selection;
+		notifyObservers(new GamerSelectedMoveEvent(moves, bestMove, stop - start));
+		return bestMove;
+	}
+
+	private int getMinScore(Role role, Move move, MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
+		// just one opponent for now
+		Role opponent = getStateMachine().getRoles().get(1);
+		List<Move> opponentMoves = getStateMachine().getLegalMoves(state, opponent);
+		int score = 100;
+		for (Move opponentMove: opponentMoves) {
+			List<Move> moves = new ArrayList<Move>();
+			moves.add(move);
+			moves.add(opponentMove);
+			// getNextState() returns the next state of the game given the current state and a joint move list containing one move per role.
+			MachineState nextState = getStateMachine().getNextState(state, moves);
+			int result = getMaxScore(role, nextState);
+			if (result < score) {
+				score = result;
+			}
+		}
+		return score;
+	}
+
+	private int getMaxScore(Role role, MachineState state) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
+		if (getStateMachine().isTerminal(state)) {
+			return getStateMachine().getGoal(state, role);
+		}
+		List<Move> moves = getStateMachine().getLegalMoves(state, role);
+		int score = 0;
+		for (Move move: moves) {
+			// Minimax alternates between getting max and min scores
+			int result = getMinScore(role, move, state);
+			if (result > score) {
+				score = result;
+			}
+		}
+		return score;
 	}
 
 	@Override
