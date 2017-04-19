d76be12a221e824601e1ca6284969a9a674f226f
Kevin Zhai <kevzhai@users.noreply.github.com>
2017-04-18 18:23:46 -0700

    wasn't actually connecting...had to return new CachedStateMachine for getInitialState intead of null. Cleaned up code a bit

--- src/main/java/MiniMaxPlayer.java
+++ src/main/java/MiniMaxPlayer.java
@@ -11,22 +11,17 @@ import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 public class MiniMaxPlayer extends StateMachineGamer {
 
 	@Override
 	public StateMachine getInitialStateMachine() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void stateMachineMetaGame(long timeout)
-			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-		// TODO Auto-generated method stub
-
+		return new CachedStateMachine(new ProverStateMachine());
 	}
 
 	@Override
@@ -35,15 +30,15 @@ public class MiniMaxPlayer extends StateMachineGamer {
 		// TODO Auto-generated method stub
 		long start = System.currentTimeMillis();
 
-		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-		Move bestMove = (moves.get(0));
-		int bestScore = 0;
 		Role role = getRole();
 		MachineState currentState = getCurrentState();
+		List<Move> moves = getStateMachine().getLegalMoves(currentState, role);
+		Move bestMove = moves.get(0);
+		int bestScore = 0;
 		for (Move move: moves) {
 			int result = getMinScore(role, move, currentState);
 			if (result > bestScore) {
-				result = bestScore;
+				bestScore = result;
 				bestMove = move;
 			}
 		}
@@ -56,15 +51,17 @@ public class MiniMaxPlayer extends StateMachineGamer {
 
 	private int getMinScore(Role role, Move move, MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
 		// just one opponent for now
-		Role opponent = getStateMachine().getRoles().get(1);
-		List<Move> opponentMoves = getStateMachine().getLegalMoves(state, opponent);
+		// https://piazza.com/class/ixl3tqyvphf6fu?cid=72
+		StateMachine machine = getStateMachine();
+		Role opponent = machine.getRoles().get(1);
+		List<Move> opponentMoves = machine.getLegalMoves(state, opponent);
 		int score = 100;
 		for (Move opponentMove: opponentMoves) {
 			List<Move> moves = new ArrayList<Move>();
 			moves.add(move);
 			moves.add(opponentMove);
 			// getNextState() returns the next state of the game given the current state and a joint move list containing one move per role.
-			MachineState nextState = getStateMachine().getNextState(state, moves);
+			MachineState nextState = machine.getNextState(state, moves);
 			int result = getMaxScore(role, nextState);
 			if (result < score) {
 				score = result;
@@ -74,10 +71,11 @@ public class MiniMaxPlayer extends StateMachineGamer {
 	}
 
 	private int getMaxScore(Role role, MachineState state) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
+		StateMachine machine = getStateMachine();
 		if (getStateMachine().isTerminal(state)) {
-			return getStateMachine().getGoal(state, role);
+			return machine.getGoal(state, role);
 		}
-		List<Move> moves = getStateMachine().getLegalMoves(state, role);
+		List<Move> moves = machine.getLegalMoves(state, role);
 		int score = 0;
 		for (Move move: moves) {
 			// Minimax alternates between getting max and min scores
@@ -116,6 +114,13 @@ public class MiniMaxPlayer extends StateMachineGamer {
 	public String getName() {
 		// TODO Auto-generated method stub
 		return "Kilimanjaro MiniMax";
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+
 	}
 
 }
